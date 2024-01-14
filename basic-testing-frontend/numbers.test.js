44352cd7bbd0f34d32dbb996f12c0c14788d08d9
Marina_Pasternak <256pasternakmarina@gmail.com>
2024-01-14 20:23:46 +0200

    added tests for numbers file

--- basic-testing-frontend/numbers.test.js
+++ basic-testing-frontend/numbers.test.js
@@ -1,5 +1,5 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from './src/util/numbers';
+import { it, expect, describe } from "vitest";
+import { transformToNumber, cleanNumbers, describe } from './src/util/numbers';
 
 it('should transform string to number', () => {
     const stringNumber = '1';
@@ -18,3 +18,20 @@ it('should yield NaN for non-transformable values', () => {
     expect(resultEmptyObject).toBeNaN();
 });
 
+describe('cleanNumbers()', () => {
+    it('should return an array of number values if an array of string number values is provided', () => {
+      const numberValues = ['1', '2'];
+  
+      const cleanedNumbers = cleanNumbers(numberValues);
+  
+      expect(cleanedNumbers[0]).toBeTypeOf('number');
+    });
+
+    it('should throw an error if an array with at least one empty string is provided', () => {
+        const numberValues = ['', 1];
+    
+        const cleanFn = () => cleanNumbers(numberValues);
+    
+        expect(cleanFn).toThrow();
+    })
+});
